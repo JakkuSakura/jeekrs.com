455013efd49e868b4cf62a1a7a90b0952ba22342
JakkuSakura <qjk2001@gmail.com>
2024-07-05 00:24:03 +0800

    feat: update deploy script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,10 +2,10 @@
 
 HOST="jeekrs.com"
 TEMP_DIR="jeekrs.com"
-DIR="/var/www/html"
+DIR="/var/www/html/jeekrs.com"
 
 pnpm run build
 
 
 (cd public && rsync -avizh --delete * "$HOST:$TEMP_DIR")
-ssh "$HOST" "sudo rsync -avizh --delete $TEMP_DIR/* $DIR"
+ssh "$HOST" "sudo rsync -avizh --delete $TEMP_DIR/. $DIR"
